3e0693a140e6055c3f947a15dea75174c63eba12
Mauricio Silva <mauriciospta@gmail.com>
2022-04-26 09:25:21 -0300

    adiciona funcoes para auxiliar nos testes

---
+++ src/helpers/teste/setValorInput.ts
@@ -0,0 +1,6 @@
+import { fireEvent } from "@testing-library/react";
+
+export const setValorInput = (input: HTMLElement, value: string) => {
+  fireEvent.change(input, { target: { value } });
+  fireEvent.blur(input);
+};
