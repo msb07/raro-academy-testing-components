a09ff8c070fecbd188ba2b4fb0694ffb9ba0e2fc
Mauricio Silva <mauriciospta@gmail.com>
2022-04-28 15:21:41 -0300

    adicona testes restantes

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FormEvent, useCallback, useMemo } from "react";
+import { FormEvent, useCallback, useMemo, useState } from "react";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { campoObrigatorio } from "../../helpers/validators/campoObrigatorio";
@@ -8,49 +8,63 @@ import { senhaValida } from "../../helpers/validators/senhaValida";
 import { useValidatedField } from "../../hooks/useValidatedField";
 
 export const Cadastro = () => {
-  const nome = useValidatedField(campoObrigatorio('Nome'));
-  const email = useValidatedField(emailValido('E-mail'));
-  const codigoAcesso = useValidatedField(campoObrigatorio('Codigo Acesso'));
-  const senha = useValidatedField(senhaValida('Senha'));
-  const validaConfirmacaoSenha = useCallback((value: string) => {
-    if (value !== senha.value) {
-      return ['Senhas não conferem'];
-    }
+  const [sucesso, setSucesso] = useState("");
+  const nome = useValidatedField(campoObrigatorio("Nome"));
+  const email = useValidatedField(emailValido("E-mail"));
+  const codigoAcesso = useValidatedField(campoObrigatorio("Codigo Acesso"));
+  const senha = useValidatedField(senhaValida("Senha"));
+  const validaConfirmacaoSenha = useCallback(
+    (value: string) => {
+      if (value !== senha.value) {
+        return ["Senhas não conferem"];
+      }
 
-    return [];
-
-  }, [senha.value]);
+      return [];
+    },
+    [senha.value]
+  );
   const confirmacaoSenha = useValidatedField(validaConfirmacaoSenha);
 
-  const formValido = useMemo(() =>
-    nome.isValid &&
-    email.isValid &&
-    codigoAcesso.isValid &&
-    senha.isValid &&
-    confirmacaoSenha.isValid
-  , [codigoAcesso.isValid, confirmacaoSenha.isValid, email.isValid, nome.isValid, senha.isValid]);
+  const formValido = useMemo(
+    () =>
+      nome.isValid &&
+      email.isValid &&
+      codigoAcesso.isValid &&
+      senha.isValid &&
+      confirmacaoSenha.isValid,
+    [
+      codigoAcesso.isValid,
+      confirmacaoSenha.isValid,
+      email.isValid,
+      nome.isValid,
+      senha.isValid,
+    ]
+  );
 
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const usuario = {
       nome: nome.value,
       email: email.value,
       senha: senha.value,
-      codigoAcesso: codigoAcesso.value
+      codigoAcesso: codigoAcesso.value,
     };
 
-    await axios.post(
-      'https://3.221.159.196:3320/auth/login',
-      usuario
-    );
-  }
+    try {
+      await axios.post("https://3.221.159.196:3320/auth/login", usuario);
+      setSucesso("Usuario cadastrado com sucesso");
+    } catch (error) {
+      console.log("first");
+      // throw new Error("msg");
+      // return {};
+      throw error;
+    }
+  };
 
   return (
     <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow-2xl sm:rounded-lg sm:px-10">
-
           <form onSubmit={onSubmit}>
             <div className="mb-5">
               <Input
@@ -101,7 +115,7 @@ export const Cadastro = () => {
                 {...codigoAcesso}
               />
             </div>
-
+            <span className="font-small text-green-600">{sucesso} </span>
             <Button type="submit" disabled={!formValido}>
               Cadastrar
             </Button>
--- src/pages/__tests__/Cadastro.test.tsx
+++ src/pages/__tests__/Cadastro.test.tsx
@@ -1,34 +1,173 @@
-import { Cadastro } from '../Cadastro';
+import { Cadastro } from "../Cadastro";
 
-import { render } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+import faker from "@faker-js/faker";
+import { validaErroApresentadoEmTela } from "../../helpers/teste/validaErroApresentadoEmTela";
+import { validaErroNaoApresentadoEmTela } from "../../helpers/teste/validaErroNaoApresentadoEmTela";
+import { setValorInput } from "../../helpers/teste/setValorInput";
+import axios from "axios";
 
 const makeSut = () => {
-  return render(
-    <Cadastro />
-  );
-}
+  return render(<Cadastro />);
+};
 
-describe('Cadastro Page', () => {
+describe("Cadastro Page", () => {
   beforeEach(jest.clearAllMocks);
   beforeEach(makeSut);
 
-  it('deve preencher todos os campos para submeter o form', () => {});
+  it("deve preencher todos os campos para submeter o form", () => {
+    const button = screen.getByText("Cadastrar");
 
-  it('deve validar o formato de e-mail no cadastro', () => {});
-
-  it('deve validar os critérios de aceitação da senha', () => {
-    // senha deve ter 8 dígitos ou mais
-    // senha deve ter letra maiuscula
-    // senha deve ter letra minúscula
-    // senha deve ter números
-    // senha deve ter caracteres especiais
+    expect(button).toBeDisabled();
   });
 
-  it('deve garantir que senha e confirmação sejam iguais', () => {});
+  it("deve validar o formato de e-mail no cadastro", () => {
+    const input = screen.getByPlaceholderText("e-mail");
+    const value = faker.internet.email();
+    const mensagemDeValidacao = "Formato de e-mail inválido";
+    validaErroApresentadoEmTela(input, mensagemDeValidacao);
+    validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+  });
 
-  it('deve enviar o formulário se todos os dados estiverem preenchidos corretamente', () => {});
+  describe("deve validar os critérios de aceitação da senha", () => {
+    let input: HTMLElement;
+    beforeEach(() => {
+      input = screen.getByPlaceholderText("Senha");
+    });
 
-  it('deve notificar o usuário que o cadastro foi efetuado com sucesso', () => {});
+    it("senha deve ter 8 dígitos ou mais", () => {
+      const value = faker.lorem.paragraph();
+      const mensagemDeValidacao = "Senha deve ter ao menos 8 caracteres";
+      validaErroApresentadoEmTela(input, mensagemDeValidacao);
+      validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+    });
 
-  it('deve apresentar os erros de validação para o usuário, caso a API retorne erro', () => {});
+    it("senha deve ter letra maiuscula", () => {
+      const value = "Teste";
+      const mensagemDeValidacao =
+        "Senha deve conter pelo menos uma letra maiúscula";
+      validaErroApresentadoEmTela(input, mensagemDeValidacao);
+      validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+    });
+
+    it("senha deve ter letra minúscula", () => {
+      const value = "Teste";
+      const mensagemDeValidacao =
+        "Senha deve conter pelo menos uma letra minúscula";
+      validaErroApresentadoEmTela(input, mensagemDeValidacao);
+      validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+    });
+
+    it("senha deve ter números", () => {
+      const value = "Teste 1";
+      const mensagemDeValidacao = "Senha deve conter pelo menos um número";
+      validaErroApresentadoEmTela(input, mensagemDeValidacao);
+      validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+    });
+
+    it("senha deve ter caracteres especiais", () => {
+      const value = "Teste@1";
+      const mensagemDeValidacao =
+        "Senha deve conter pelo menos um caractere especial";
+      validaErroApresentadoEmTela(input, mensagemDeValidacao);
+      validaErroNaoApresentadoEmTela(input, value, mensagemDeValidacao);
+    });
+  });
+
+  it("deve garantir que senha e confirmação sejam iguais", () => {
+    const senha = screen.getByPlaceholderText("Senha");
+    const confirmacaoSenha = screen.getByPlaceholderText(
+      "Confirmação de Senha"
+    );
+    const mensagemErro = "Senhas não conferem";
+    const senhaEscolhida = "123";
+    const senhaErrada = "1234";
+
+    // Testa se a senha escolhida no campo senha é igual a do campo Confiramção de Senha
+    setValorInput(senha, senhaEscolhida);
+    validaErroNaoApresentadoEmTela(
+      confirmacaoSenha,
+      senhaEscolhida,
+      mensagemErro
+    );
+
+    // Modifica o valor do campo Confiramação de Senha e testa se o erro é renderizado na tela
+    setValorInput(confirmacaoSenha, senhaErrada);
+    validaErroApresentadoEmTela(confirmacaoSenha, mensagemErro);
+  });
+
+  it("deve enviar o formulário se todos os dados estiverem preenchidos corretamente", () => {
+    jest.spyOn(axios, "post").mockResolvedValue("ok");
+    const nome = screen.getByPlaceholderText("Nome");
+    const email = screen.getByPlaceholderText("e-mail");
+    const senha = screen.getByPlaceholderText("Senha");
+    const confirmacaoSenha = screen.getByPlaceholderText(
+      "Confirmação de Senha"
+    );
+    const codigoAcesso = screen.getByPlaceholderText("Código de Acesso");
+    const botao = screen.getByText("Cadastrar");
+    const dados = {
+      nome: faker.name.firstName(),
+      email: faker.internet.email(),
+      senha: "S3nh@!123",
+      codigoAcesso: faker.lorem.paragraph(),
+    };
+
+    // construcao
+    setValorInput(nome, dados.nome);
+    setValorInput(email, dados.email);
+    setValorInput(senha, dados.senha);
+    setValorInput(confirmacaoSenha, dados.senha);
+    setValorInput(codigoAcesso, dados.codigoAcesso);
+    botao.click();
+
+    // asserts
+    expect(axios.post).toHaveBeenCalled();
+  });
+
+  it("deve notificar o usuário que o cadastro foi efetuado com sucesso", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue("ok");
+    const nome = screen.getByPlaceholderText("Nome");
+    const email = screen.getByPlaceholderText("e-mail");
+    const senha = screen.getByPlaceholderText("Senha");
+    const confirmacaoSenha = screen.getByPlaceholderText(
+      "Confirmação de Senha"
+    );
+    const codigoAcesso = screen.getByPlaceholderText("Código de Acesso");
+    const botao = screen.getByText("Cadastrar");
+    const dados = {
+      nome: faker.name.firstName(),
+      email: faker.internet.email(),
+      senha: "S3nh@!123",
+      codigoAcesso: faker.lorem.paragraph(),
+    };
+
+    // construcao
+    setValorInput(nome, dados.nome);
+    setValorInput(email, dados.email);
+    setValorInput(senha, dados.senha);
+    setValorInput(confirmacaoSenha, dados.senha);
+    setValorInput(codigoAcesso, dados.codigoAcesso);
+    botao.click();
+    expect(axios.post).toHaveBeenCalled();
+
+    await expect(
+      screen.getByText("Usuario cadastrado com sucesso")
+    ).toBeDefined();
+
+    // coloquei uma div pra o caso de sucesso na chamada da api porém nao entendi o motivo
+    // de nao estar encontrando no documento.
+
+    //recebo um aviso sobre mudanças de estado serem envolvidas em act(...), porém não sei como proceder
+  });
+
+  it("deve apresentar os erros de validação para o usuário, caso a API retorne erro", async () => {
+    jest.spyOn(axios, "post").mockRejectedValue("errro");
+
+    // // asserts
+    await expect(axios.post).rejects.toThrow("erro");
+
+    // nesse caso testei várias formas para tentar passar pelo catch, porém, sem sucesso
+    // alguns testes funcionam mas sem passar pelo pelo esperado
+  });
 });
